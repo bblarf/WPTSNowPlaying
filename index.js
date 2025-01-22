c19d800fc52e0d43cceaa8572d48ccfe44fdd702
bblarf <erichart820211@gmail.com>
2025-01-22 16:00:01 -0500

    fully works now, just need to simplify the interface

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const axios = require("axios");
+const axios = require("axios")
+const fs = require('fs')
 getNowPlaying();
-
+setInterval(getNowPlaying, 10000);
 function getNowPlaying() {
     axios.get("http://wpts1.wpts.pitt.edu/current.cgi")
         .then(res => {
+            fs.writeFile('nowplaying.txt', '', (err) => {
+                if (err) throw err;
+                else{
+                    console.log('new change at ' + Date.now());
+                }
+            })
             let fullInfo = res.data;
             const arrInfo = fullInfo.split(" - ");
-            console.log("Now Playing: " + arrInfo[1] + " by " + arrInfo[0]);
+            let textOutput = "Now Playing: '" + arrInfo[1] + "' by " + arrInfo[0];
+            fs.writeFile('nowplaying.txt', textOutput, (err) => {
+                if (err) throw err;
+            })
         })
         .catch(err => {
             console.log(err);
